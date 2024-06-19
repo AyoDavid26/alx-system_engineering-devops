1c460ce04f932b42e8dfce4c220a06d22c70a54d
AyoDavid26 <folayanayodeji@gmail.com>
2024-06-19 12:15:20 -0700

    More-API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that queries Reddit API and returns number of subscribers
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {"User-Agent": "Mozilla/7.0.0"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.Request.Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,2 @@
+This is the Readme file for the ALX 0X16 Advanced Api Project
+By AyoDavid26
